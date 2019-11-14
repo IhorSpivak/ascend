110634a8eea00914ae9d5197042dc82e578b4710
Pavlo Nikitin <pawlo2102@gmail.com>
2019-11-14 20:08:28 +0200

    sign up model

---
+++ modules/domain/entity/src/main/java/com/doneit/ascend/domain/entity/SignUpModel.kt
@@ -0,0 +1,9 @@
+package com.doneit.ascend.domain.entity
+
+class SignUpModel(
+    var email: String = "",
+    var phone: String = "",
+    var name: String = "",
+    var password: String = "",
+    var passwordConfirmation: String = ""
+)
--- modules/features/featureLogin/src/main/java/com/doneit/ascend/presentation/login/sign_up/SignUpVIewModel.kt
+++ modules/features/featureLogin/src/main/java/com/doneit/ascend/presentation/login/sign_up/SignUpVIewModel.kt
@@ -2,13 +2,14 @@ package com.doneit.ascend.presentation.login.sign_up
 
 import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
+import com.doneit.ascend.domain.entity.SignUpModel
 import com.vrgsoft.core.presentation.fragment.BaseViewModel
 
 class SignUpViewModel(
 
 ) {
 
-    //val registrationModel = Registration()
+    val registrationModel = SignUpModel()
 
     val registrationEvent =  MutableLiveData<Boolean>()
 
