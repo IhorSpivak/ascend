048872acc7efae40bffd3c8ddc9e3de0baa6727c
Pavlo Nikitin <pawlo2102@gmail.com>
2019-12-21 13:30:06 +0200

    camera permissions

--- modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/create_group/CreateGroupFragment.kt
+++ modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/create_group/CreateGroupFragment.kt
@@ -99,7 +99,8 @@ class CreateGroupFragment : ArgumentedFragment<FragmentCreateGroupBinding, Creat
         EzPermission.with(context!!)
             .permissions(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
             )
             .request { granted, _, _ ->
                 if (granted.contains(Manifest.permission.READ_EXTERNAL_STORAGE)) {
