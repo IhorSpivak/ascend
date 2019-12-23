d6f6f7b0b7d3e8e9df8eacdc77c72065776dd4e5
Pavlo Nikitin <pawlo2102@gmail.com>
2019-12-23 16:46:21 +0200

    fix notification date format

--- modules/domain/gateway/src/main/java/com/doneit/ascend/domain/gateway/common/mapper/to_entity/DateMapper.kt
+++ modules/domain/gateway/src/main/java/com/doneit/ascend/domain/gateway/common/mapper/to_entity/DateMapper.kt
@@ -3,8 +3,11 @@ package com.doneit.ascend.domain.gateway.common.mapper.to_entity
 import java.text.SimpleDateFormat
 import java.util.*
 
-private const val REMOTE_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+private const val REMOTE_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"//considered always to have 0 timezone offset
 
 fun String.toDate(): Date? {
-    return SimpleDateFormat(REMOTE_DATE_FORMAT, Locale.getDefault()).parse(this)
+    val formatter = SimpleDateFormat(REMOTE_DATE_FORMAT, Locale.getDefault())
+    formatter.timeZone = TimeZone.getTimeZone("GMT")
+
+    return formatter.parse(this)
 }
