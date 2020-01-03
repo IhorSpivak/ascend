ca2f7301b33eab377c69c51c7afd364f600510f7
Pavlo Nikitin <pawlo2102@gmail.com>
2020-01-03 19:31:06 +0200

    nullable full name

--- modules/domain/entity/src/main/java/com/doneit/ascend/domain/entity/RateEntity.kt
+++ modules/domain/entity/src/main/java/com/doneit/ascend/domain/entity/RateEntity.kt
@@ -5,7 +5,7 @@ import java.util.*
 data class RateEntity(
     val value: Float,
     val userId: Long,
-    val fullName: String,
+    val fullName: String?,
     val image: ImageEntity,
     val createdAt: Date,
     val updatedAt: Date?
