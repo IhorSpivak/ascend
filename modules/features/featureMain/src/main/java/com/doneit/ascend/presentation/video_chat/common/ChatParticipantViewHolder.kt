7fc78a6950f49e4e487f9f5d9083446c2531edff
Pavlo Nikitin <pawlo2102@gmail.com>
2020-02-19 11:44:09 +0200

    avatar changes

--- modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/video_chat/common/ChatParticipantViewHolder.kt
+++ modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/video_chat/common/ChatParticipantViewHolder.kt
@@ -24,6 +24,7 @@ class ChatParticipantViewHolder(
         binding.name = model.fullName
         binding.isHandRisen = model.isHandRisen
         binding.isSpeaker = model.isSpeaker
+        binding.isMicroOff = model.isMuted
 
         lastModel?.get()?.getVideoTrack()?.removeRenderer(binding.videoView)
         model.getVideoTrack()?.let {
