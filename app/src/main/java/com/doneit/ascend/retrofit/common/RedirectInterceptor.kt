a6556252c4ad4d01f8bb632ec2043397c7e4daa3
Pavlo Nikitin <pawlo2102@gmail.com>
2020-05-07 19:55:40 +0300

    wrong login crash fix

--- app/src/main/java/com/doneit/ascend/retrofit/common/RedirectInterceptor.kt
+++ app/src/main/java/com/doneit/ascend/retrofit/common/RedirectInterceptor.kt
@@ -1,6 +1,5 @@
 package com.doneit.ascend.retrofit.common
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import com.doneit.ascend.presentation.login.LogInActivity
@@ -15,7 +14,8 @@ internal class RedirectInterceptor(
         val newRequest = chain.request().newBuilder().build()
 
         val response = chain.proceed(newRequest)
-        if (response.code == 401) {
+            //todo: hotfix of 401 handling with checking login url (refactor)
+        if (response.code == 401 && newRequest.url.toString() != "https://ascend-backend.herokuapp.com/api/v1/sessions") {
             val intent = Intent(context, LogInActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
