70dc72d31e1b537097ac73163c9e3b9211532fb8
Pavlo Nikitin <pawlo2102@gmail.com>
2020-05-08 12:52:10 +0300

    validation fix (if something go wrong revert it)

--- modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/create_group/CreateGroupViewModel.kt
+++ modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/create_group/CreateGroupViewModel.kt
@@ -123,7 +123,7 @@ class CreateGroupViewModel(
             if (s.isValidMeetingsNumber().not()) {
                 result.isSucceed = false
                 result.errors.add(R.string.error_number_of_meetings)
-            } else if (createGroupModel.scheduleDays.size != s.toInt()) {
+            } else if (createGroupModel.scheduleDays.size > s.toInt()) {
                 result.isSucceed = false
                 result.errors.add(R.string.error_number_of_meetings_count)
             }
