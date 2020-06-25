55a530c04351c4236783d1932b721c58103df806
Pavlo Nikitin <pawlo2102@gmail.com>
2020-06-25 14:52:08 +0300

    members clear in workshop group

--- modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/create_group/master_mind/CreateMMGroupFragment.kt
+++ modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/create_group/master_mind/CreateMMGroupFragment.kt
@@ -41,17 +41,17 @@ class CreateMMGroupFragment : ArgumentedFragment<FragmentCreateMmGroupBinding, C
     override fun viewCreated(savedInstanceState: Bundle?) {
         binding.model = viewModel
         GroupAction.values().forEach {
-            if (arguments!!.containsKey(it.toString())){
+            if (arguments!!.containsKey(it.toString())) {
                 group = arguments!!.getParcelable(it.toString())
-                if (group != null){
+                if (group != null) {
                     what = it.toString()
                 }
             }
         }
         binding.apply {
-            actionTitle = if (what == null){
+            actionTitle = if (what == null) {
                 getString(R.string.create_create)
-            }else{
+            } else {
                 what!!.capitalize()
             }
         }
@@ -64,14 +64,14 @@ class CreateMMGroupFragment : ArgumentedFragment<FragmentCreateMmGroupBinding, C
             handleNavigation(it)
         })
         viewModel.members.observe(this, Observer {
-            viewModel.createGroupModel.participants.set(it.filter {attendee ->
+            viewModel.createGroupModel.participants.set(it.filter { attendee ->
                 !attendee.isAttended
-            }.map {attendee ->
+            }.map { attendee ->
                 attendee.email.orEmpty()
             })
         })
         viewModel.membersToDelete.observe(this, Observer {
-            viewModel.createGroupModel.participantsToDelete.set(it.map {attendee ->
+            viewModel.createGroupModel.participantsToDelete.set(it.map { attendee ->
                 attendee.email.orEmpty()
             })
         })
@@ -86,7 +86,7 @@ class CreateMMGroupFragment : ArgumentedFragment<FragmentCreateMmGroupBinding, C
                 }
             }
         })
-        if (group != null){
+        if (group != null) {
             binding.btbComplete.apply {
                 text = getString(R.string.btn_save_action)
                 setOnClickListener {
@@ -96,8 +96,8 @@ class CreateMMGroupFragment : ArgumentedFragment<FragmentCreateMmGroupBinding, C
                     }
                 }
             }
-            when(group!!.groupType){
-                GroupType.INDIVIDUAL ->{
+            when (group!!.groupType) {
+                GroupType.INDIVIDUAL -> {
                     viewModel.createGroupModel.isPrivate.set(true)
                 }
                 GroupType.MASTER_MIND -> {
@@ -105,9 +105,13 @@ class CreateMMGroupFragment : ArgumentedFragment<FragmentCreateMmGroupBinding, C
                 }
             }
             viewModel.createGroupModel.apply {
-                when(what){
-                    GroupAction.DUPLICATE.toString() ->{name.observableField.set(group!!.name.plus("(2)"))}
-                    GroupAction.EDIT.toString() ->{name.observableField.set(group!!.name)}
+                when (what) {
+                    GroupAction.DUPLICATE.toString() -> {
+                        name.observableField.set(group!!.name.plus("(2)"))
+                    }
+                    GroupAction.EDIT.toString() -> {
+                        name.observableField.set(group!!.name)
+                    }
                 }
                 numberOfMeetings.observableField.set(group!!.meetingsCount.toString())
                 price.observableField.set(group!!.price.toString())
@@ -130,7 +134,7 @@ class CreateMMGroupFragment : ArgumentedFragment<FragmentCreateMmGroupBinding, C
                 Glide.with(context!!)
                     .asBitmap()
                     .load(group!!.image!!.url)
-                    .into(object : SimpleTarget<Bitmap>(){
+                    .into(object : SimpleTarget<Bitmap>() {
                         override fun onResourceReady(
                             resource: Bitmap,
                             transition: Transition<in Bitmap>?
@@ -140,11 +144,11 @@ class CreateMMGroupFragment : ArgumentedFragment<FragmentCreateMmGroupBinding, C
 
                     })
             }
-            viewModel.apply{
+            viewModel.apply {
                 members.postValue(group!!.attendees?.toMutableList())
-                selectedMembers.addAll(group!!.attendees?: emptyList())
+                selectedMembers.addAll(group!!.attendees ?: emptyList())
             }
-        }else{
+        } else {
             binding.btbComplete.setOnClickListener {
                 viewModel.completeClick()
             }
@@ -158,17 +162,13 @@ class CreateMMGroupFragment : ArgumentedFragment<FragmentCreateMmGroupBinding, C
                 CreateGroupFragment()
             }
             CreateMMGroupContract.Navigation.TO_INDIVIDUAL -> {
-                viewModel.apply {
-                    if (selectedMembers.size > 1){
-                        val temp = selectedMembers.first()
-                        selectedMembers.clear()
-                        selectedMembers.add(temp)
-                        members.postValue(selectedMembers)
-                    }
-                }
                 viewModel.createGroupModel.groupType = GroupType.INDIVIDUAL
                 IndividualGroupFragment(group)
             }
+        }
+        viewModel.apply {
+            selectedMembers.clear()
+            members.postValue(selectedMembers)
         }
 
         childFragmentManager.replace(R.id.container, fragment)
