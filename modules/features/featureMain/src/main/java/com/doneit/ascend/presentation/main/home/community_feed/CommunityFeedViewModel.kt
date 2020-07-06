24eba698f91d5e7e56f3f0f420dcce367ab30574
Pavlo Nikitin <pawlo2102@gmail.com>
2020-07-06 19:40:56 +0300

    community feed update attachments

--- modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/home/community_feed/CommunityFeedViewModel.kt
+++ modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/home/community_feed/CommunityFeedViewModel.kt
@@ -1,18 +1,24 @@
 package com.doneit.ascend.presentation.main.home.community_feed
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.doneit.ascend.domain.entity.community_feed.Channel
 import com.doneit.ascend.domain.entity.dto.CommunityFeedDTO
 import com.doneit.ascend.domain.entity.dto.SortType
+import com.doneit.ascend.domain.entity.user.UserEntity
 import com.doneit.ascend.domain.use_case.interactor.community_feed.CommunityFeedUseCase
+import com.doneit.ascend.domain.use_case.interactor.user.UserUseCase
 import com.doneit.ascend.presentation.main.base.BaseViewModelImpl
 import com.vrgsoft.annotations.CreateFactory
 import com.vrgsoft.annotations.ViewModelDiModule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @CreateFactory
 @ViewModelDiModule
 class CommunityFeedViewModel(
-    private val postsUseCase: CommunityFeedUseCase
+    private val postsUseCase: CommunityFeedUseCase,
+    private val userUseCase: UserUseCase
 ) : BaseViewModelImpl(), CommunityFeedContract.ViewModel {
     override val posts = postsUseCase.loadPosts(
         viewModelScope, CommunityFeedDTO(
@@ -27,6 +33,14 @@ class CommunityFeedViewModel(
             sortType = SortType.DESC
         )
     )
+
+    override val user = MutableLiveData<UserEntity>()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            user.postValue(userUseCase.getUser())
+        }
+    }
 
     override fun onNewPostClick() {
     }
--- modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/home/community_feed/common/PostViewHolder.kt
+++ modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/home/community_feed/common/PostViewHolder.kt
@@ -1,13 +1,22 @@
 package com.doneit.ascend.presentation.main.home.community_feed.common
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
+import com.doneit.ascend.domain.entity.community_feed.Attachment
 import com.doneit.ascend.domain.entity.community_feed.Post
 import com.doneit.ascend.presentation.main.R
+import com.doneit.ascend.presentation.main.common.gone
+import com.doneit.ascend.presentation.main.common.visible
 import com.doneit.ascend.presentation.main.databinding.ListItemFeedBinding
+import com.doneit.ascend.presentation.utils.extensions.visibleOrGone
 
 class PostViewHolder(
     itemView: View,
@@ -19,7 +28,7 @@ class PostViewHolder(
     fun bind(post: Post) {
         with(binding) {
             postModel = post
-            mivAttachments.setAttachments(post.attachments)
+            setupAttachments(post.attachments)
             mmiAvatar.setOnClickListener { postClickListeners.onUserClick(1L) }
             tvName.setOnClickListener { postClickListeners.onUserClick(1L) }
             btnLike.setOnClickListener { postClickListeners.onLikeClick(post.id) }
@@ -31,6 +40,27 @@ class PostViewHolder(
                 )
             }
         }
+    }
+
+    private fun ListItemFeedBinding.setupAttachments(attachments: List<Attachment>) {
+        if (attachments.isEmpty()) mivAttachments.gone()
+        imvFirst.glideLoad(attachments.getOrNull(0))
+        imvSecond.glideLoad(attachments.getOrNull(1))
+        imvThird.glideLoad(attachments.getOrNull(2))
+    }
+
+    private fun ImageView.glideLoad(attachment: Attachment?) {
+        visibleOrGone(attachment == null)
+        attachment ?: return
+        binding.mivAttachments.visible()
+        Glide.with(this)
+            .asBitmap()
+            .load(attachment.url)
+            .error(R.drawable.ic_action_block)
+            .centerCrop()
+            .placeholder(ColorDrawable(Color.LTGRAY))
+            .transition(BitmapTransitionOptions.withCrossFade())
+            .into(this)
     }
 
     companion object {
