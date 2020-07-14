99a6af37d41fcb4e002eb3fadaa0a6537b549387
Pavlo Nikitin <pawlo2102@gmail.com>
2020-07-14 17:39:44 +0300

    share post screen dimension changed (fullscreen with padding)

--- modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/home/community_feed/share_post/SharePostBottomSheetFragment.kt
+++ modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/main/home/community_feed/share_post/SharePostBottomSheetFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
+import android.widget.FrameLayout
 import androidx.databinding.DataBindingUtil
 import com.doneit.ascend.domain.entity.user.UserEntity
 import com.doneit.ascend.presentation.main.R
 import com.doneit.ascend.presentation.main.databinding.FragmentSharePostBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
@@ -16,6 +20,7 @@ import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
+
 
 class SharePostBottomSheetFragment : BottomSheetDialogFragment(), KodeinAware {
 
@@ -48,11 +53,37 @@ class SharePostBottomSheetFragment : BottomSheetDialogFragment(), KodeinAware {
             container,
             false
         )
+        dialog!!.setOnShowListener { dialog ->
+            val d = dialog as BottomSheetDialog
+            val bottomSheetInternal =
+                d.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+            bottomSheetInternal?.layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
+            BottomSheetBehavior.from<View?>(bottomSheetInternal!!).state =
+                BottomSheetBehavior.STATE_EXPANDED
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //TODO: refactor if possible, for opening view on full screen
+        view.viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                view.viewTreeObserver.removeOnGlobalLayoutListener(this)
+
+                val dialog = dialog as BottomSheetDialog
+                val bottomSheet =
+                    dialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout?
+                val behavior = BottomSheetBehavior.from(bottomSheet!!)
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+                val newHeight = activity?.window?.decorView?.measuredHeight
+                val viewGroupLayoutParams = bottomSheet.layoutParams
+                viewGroupLayoutParams.height = newHeight ?: 0
+                bottomSheet.layoutParams = viewGroupLayoutParams
+            }
+        })
         binding.model = viewModel
         binding.apply {
             lifecycleOwner = this@SharePostBottomSheetFragment
