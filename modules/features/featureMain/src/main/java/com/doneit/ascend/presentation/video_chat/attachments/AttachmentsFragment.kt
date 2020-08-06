d05ed2e21ae22b4b6d004bb36039b87814ced329
Dr0gis <dr0gis@mail.ru>
2020-08-06 16:33:35 +0300

    [AS-391] save attachment to download folder

--- modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/video_chat/attachments/AttachmentsFragment.kt
+++ modules/features/featureMain/src/main/java/com/doneit/ascend/presentation/video_chat/attachments/AttachmentsFragment.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.widget.Toast
+import androidx.core.net.toUri
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.amazonaws.auth.CognitoCachingCredentialsProvider
@@ -75,7 +76,7 @@ class AttachmentsFragment : BaseFragment<FragmentAttachmentsBinding>() {
         }
 
         bind<ViewModelProvider.Factory>(tag = AttachmentsFragment::class.java) with singleton {
-            AttachmentsViewModelFactory(instance(), instance(), instance(), instance())
+            AttachmentsViewModelFactory(instance(), instance(), instance(), instance(), instance())
         }
 
         bind<AttachmentsContract.ViewModel>() with provider {
@@ -129,6 +130,10 @@ class AttachmentsFragment : BaseFragment<FragmentAttachmentsBinding>() {
 
         viewModel.transferEvents.observe(viewLifecycleOwner, Observer {
             Toast.makeText(requireContext(), getString(it.messageRes), Toast.LENGTH_LONG).show()
+        })
+
+        viewModel.showPreview.observe(viewLifecycleOwner, Observer {  file ->
+            binding.bigImage.showImage(file.toUri())
         })
 
         val groupId = arguments!!.getParcelable<AttachmentsArg>(ATTACHMENTS_ARGS)!!.groupId
