79fdb869f03a3d9a131f9b26a6f5bd9b81b6e3df
IhorSpivaK <haterkyiv@gmail.com>
2020-08-17 17:30:24 +0300

    Read more fixed get Push notifications (redirect to screens in progress) Community fixed

--- app/src/main/java/com/doneit/ascend/push/AscendFirebaseMessagingService.kt
+++ app/src/main/java/com/doneit/ascend/push/AscendFirebaseMessagingService.kt
@@ -46,15 +46,18 @@ class AscendFirebaseMessagingService : FirebaseMessagingService(), KodeinAware {
 
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
-        Log.d("firebase", p0.toString())
+        Log.d("CHECK_KEY_MESSAGE", p0.toString())
         p0.notification?.let {
-            if (p0.data.containsKey(GROUP_KEY)) {
-                sendNotification(it.title.orEmpty(), p0.data.getValue(GROUP_KEY).toLong())
-                useCase.notificationReceived(p0.toEntity())
-            }
-            if (p0.data.containsKey(MESSAGE_KEY)) {
-                sendNotification(it.title.orEmpty(), p0.data.getValue(GROUP_KEY).toLong())
-            }
+
+//            if (p0.data.containsKey(GROUP_KEY)) {
+//                sendNotification(it.title.orEmpty(), p0.data.getValue(GROUP_KEY).toLong())
+//                useCase.notificationReceived(p0.toEntity())
+//            }
+//            if (p0.data.containsKey(MESSAGE_KEY)) {
+//                sendNotification(it.title.orEmpty(), p0.data.getValue(GROUP_KEY).toLong())
+//            }
+            sendNotification(it.title.orEmpty())
+
         }
 
     }
@@ -66,22 +69,22 @@ class AscendFirebaseMessagingService : FirebaseMessagingService(), KodeinAware {
         }
     }
 
-    private fun sendNotification(messageTitle: String, id: Long) {
+    private fun sendNotification(messageTitle: String) {
 
-        //TODO: figure out how to show SplashActivity in case of background app start:
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra(Constants.KEY_GROUP_ID, id)
-        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-            PendingIntent.FLAG_UPDATE_CURRENT)
+//        //TODO: figure out how to show SplashActivity in case of background app start:
+//        val intent = Intent(this, MainActivity::class.java)
+//        intent.putExtra(Constants.KEY_GROUP_ID, id)
+//        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+//            PendingIntent.FLAG_UPDATE_CURRENT)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
                 //TODO: no icon for pushes:
-            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setSmallIcon(R.drawable.ic_ascend_splash_logo)
             .setContentTitle(messageTitle)
             .setContentText(null)
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
-            .setContentIntent(pendingIntent)
+//            .setContentIntent(pendingIntent)
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -95,5 +98,18 @@ class AscendFirebaseMessagingService : FirebaseMessagingService(), KodeinAware {
         private const val CHANNEL_ID = "ascend_n"
         private const val GROUP_KEY = "entity_id"
         private const val MESSAGE_KEY = "message"
+
+
+//        meeting_started
+//        new_groups
+//        invite_to_a_meeting
+//        removed_from_group
+//        ten_minutes_to_start_meeting
+//        group_cancelled
+//        start_time_changed
+//        invite_to_a_webinar
+//        invite_to_a_channel
+//        post_commented
+//        post_liked
     }
 }
